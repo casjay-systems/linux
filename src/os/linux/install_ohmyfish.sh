bc2aa9db3ae8fabb63eec7dbfc1fdd2661d92d68
Jason <jason@casjay.net>
2021-01-01 02:50:46 -0500

    🏠🐜❗ Minor Fixes ❗🐜🏠

--- src/os/linux/install_ohmyfish.sh
+++ src/os/linux/install_ohmyfish.sh
@@ -23,17 +23,19 @@ setup_fish() {
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 setup_ohmyfish() {
-  if [ -d "$HOME/.local/share/fish/oh-my-fish/.git" ] && [ ! -f "$srcdir/config/fish/install.sh" ]; then
-    execute \
-      "git -C $HOME/.local/share/fish/oh-my-fish pull -q >/dev/null 2>&1 && \
-      fish omf update " \
-      "Updating oh-my-fish"
-  else
-    if [ ! -d "$HOME/.local/share/omf" ]; then
+  if [ ! -f "$srcdir/config/fish/install.sh" ]; then
+    if [ -d "$HOME/.local/share/fish/oh-my-fish/.git" ]; then
       execute \
-        "curl -LSs github.com/oh-my-fish/oh-my-fish/raw/master/bin/install > /tmp/omf-install && \
-        fish /tmp/omf-install --noninteractive --yes" \
-        "Installing oh-my-fish"
+        "git -C $HOME/.local/share/fish/oh-my-fish pull -q >/dev/null 2>&1 && \
+        fish -c omf update" \
+        "Updating oh-my-fish"
+    else
+      if [ ! -d "$HOME/.local/share/omf" ]; then
+        execute \
+          "curl -LSs github.com/oh-my-fish/oh-my-fish/raw/master/bin/install > $srcdir/config/fish/omf-install && \
+          fish $srcdir/config/fish/omf-install --noninteractive --yes" \
+          "Installing oh-my-fish"
+      fi
     fi
   fi
 }
@@ -41,7 +43,7 @@ setup_ohmyfish() {
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 setup_fishplugins() {
-  if [ -f "$srcdir/config/fish/plugins.fish" ] && [ ! -f "$srcdir/config/fish/install.sh" ]; then
+  if [ -f "$srcdir/config/fish/plugins.fish" ]; then
     execute \
       "fish $srcdir/config/fish/plugins.fish 2>/dev/null" \
       "Installing fish plugins"
