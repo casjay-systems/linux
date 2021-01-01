820e9a6d67a7b0fc78989fb5210d9d14b9bfabf0
Jason <jason@casjay.net>
2020-12-31 22:48:04 -0500

    🏠🐜❗ Minor Fixes ❗🐜🏠

--- src/os/linux_setup.sh
+++ src/os/linux_setup.sh
@@ -37,13 +37,7 @@ NC='\033[0m'
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 # Grab the OS detection script if it doesn't exist script
-if [ -f "$srcdir/os" ]; then
-  cd "$srcdir/os" && source "utils.sh"
-else
-  clear
-  printf "\n\n${RED}Failed to source the $srcdir/os/utils.sh file${NC}\n\n"
-  exit 1
-fi
+
 if [ -f $srcdir/os/osdetect.sh ]; then
   source $srcdir/os/osdetect.sh
 else
