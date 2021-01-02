bd54d20442a30bc48905a3103c5c33598f6f1c95
casjay <git-admin@casjaysdev.com>
2021-01-02 06:44:47 -0500

    Update install_git.sh

--- src/os/linux/install_git.sh
+++ src/os/linux/install_git.sh
@@ -10,7 +10,7 @@ srcdir="$(cd .. && pwd)"
 install_git() {
   if [ -f "$srcdir/config/git/install.sh" ]; then
     execute "bash -c $srcdir/config/git/install.sh" "Installing GIT: $srcdir/config/git/install.sh"
-  elif [ -d "$srcdir/config/git " ]; then
+  elif [ -d "$srcdir/config/git" ]; then
     execute \
       "ln -sf $srcdir/config/git/gitconfig ~/.gitconfig" \
       "$srcdir/config/git/gitconfig  → ~/.gitconfig"
